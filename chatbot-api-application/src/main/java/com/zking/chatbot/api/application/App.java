62619c139ec010e06d892dce8065ed22f126347a
SY0907 <wjksy1314@qq.com>
2024-11-28 20:05:27 +0800

    sy,feat: init ddd project

---
+++ chatbot-api-application/src/main/java/com/zking/chatbot/api/application/App.java
@@ -0,0 +1,13 @@
+package com.zking.chatbot.api.application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
